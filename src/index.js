62e7cc19c5add326298ef2cc9b138942b250de8a
dkile <dkile532@naver.com>
2023-11-20 01:56:35 +0900

    Feat: 유저 인풋을 받아 콘솔에 결과 출력

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-export default function RacingCarGame() {
-}
-// export default class RacingCarGame {
-// }
+import RacingCarGame from "/src/RacingCarGame.js";
 
+const $carGameForm = document.querySelector(".car-game-form");
 
-new RacingCarGame();
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const carNames = e.target["car-names-input"].value.split(",");
+  const racingCount = e.target["racing-count-input"].value;
+  const racingCarGame = new RacingCarGame(carNames);
+  const result = racingCarGame.play(racingCount);
+  console.log(result);
+};
+
+$carGameForm.addEventListener("submit", handleSubmit);
